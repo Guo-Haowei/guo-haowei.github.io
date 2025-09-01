39a7cfaa1edab62495a1add1acd8b364070b4787
Guo-Haowei <Guo-Haowei@users.noreply.github.com>
2025-09-01 22:52:20 +0900

    Add chess project

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,98 +1,82 @@
 var app = angular.module('Portfolio', []);
 
-
 app.controller('appCtrl', function ($scope) {
     $scope.currentPage = 0;
     $scope.currentPort = getdata();
     $scope.totalPage = 5;
     $scope.count = 0;
-
-    /*
-    $scope.setPage = function (page) {
-        var oldlable = document.getElementById('filter' + $scope.currentPage.toString());
-        oldlable.className = 'w3-btn w3-white';
-        var oldpage = document.getElementById('p' + $scope.currentPage.toString());
-        oldpage.className = 'w3-hover-black'
-
-        $scope.currentPage = page;
-        $scope.currentPort = getdata()[page];
-
-        var newlable = document.getElementById('filter' + page.toString());
-        var newpage = document.getElementById('p' + page.toString());
-        newlable.className = 'w3-btn w3-black';
-        newpage.className = 'w3-black';
-    }
-    */
 });
 
-let _data = null;
 const getdata = () => {
-    if (_data === null) {
-        const tags = [
-            {
-                href: 'https://github.com/Guo-Haowei/cc',
-                img: 'project-cc.png',
-                title: 'C Compiler',
-                body: 'A self-hosting C Compiler'
-            },
-            {
-                href: 'https://github.com/Guo-Haowei/c.c',
-                img: 'project-c.png',
-                title: 'C Interpreter',
-                body: 'A self-hosting C interpreter in 1000 lines'
-            },
-            {
-                href: 'https://github.com/Guo-Haowei/GLSL-Path-Tracer',
-                img: 'project-glsl.png',
-                title: 'GLSL Path Tracer',
-                body: 'GLSL Path Tracer written in C++'
-            },
-            {
-                href: 'https://github.com/Guo-Haowei/VCT',
-                img: 'project-vct.png',
-                title: 'Voxel Cone Tracing',
-                body: 'Implementation of Voxel Cone Tracing with OpenGL and C++'
-            },
-            {
-                href: 'https://github.com/Guo-Haowei/Rasterizer',
-                img: 'project-sr.png',
-                title: 'Software Rasterizer',
-                body: 'Software Rasterizer written in C++ to learn how Graphics API work'
-            },
-            {
-                href: 'https://github.com/Guo-Haowei/PBR',
-                img: 'project-pbr.png',
-                title: 'PBR',
-                body: 'Implementing Physically based rendering with morden graphics API'
-            },
-            {
-                href: 'https://github.com/Guo-Haowei/TheAviator',
-                img: 'project-the-aviator.png',
-                title: 'The Aviator',
-                body: 'My first game written with OpenGL and C++'
-            },
-            {
-                href: 'pages/SuperCaveBoy/index.html',
-                img: 'project-super-caveboy.png',
-                title: 'Super Cave Boy',
-                body: 'A 2D platformer game written in JavaScript'
-            },
-            {
-                href: 'pages/TurnBasedGame/index.html',
-                img: 'project-turn-based.png',
-                title: 'Turn Based Game',
-                body: 'A turn based RPG game written in JavaScript'
-            }
-        ];
-        let tmp = [];
-        for (let i = 0; i < tags.length; i += 1) {
-            if (i % 3 === 0) {
-                tmp.push([]);
-            }
-            tmp[tmp.length - 1].push(tags[i]);
+    const cards = [
+        {
+            href: 'https://github.com/Guo-Haowei/BitboardX',
+            img: 'project-chess.png',
+            title: 'BitbordX',
+            body: 'A rust chess engine'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/cc',
+            img: 'project-cc.png',
+            title: 'C Compiler',
+            body: 'A self-hosting C Compiler'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/c.c',
+            img: 'project-c.png',
+            title: 'C Interpreter',
+            body: 'A self-hosting C interpreter in 1000 lines'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/GLSL-Path-Tracer',
+            img: 'project-glsl.png',
+            title: 'GLSL Path Tracer',
+            body: 'GLSL Path Tracer written in C++'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/VCT',
+            img: 'project-vct.png',
+            title: 'Voxel Cone Tracing',
+            body: 'Implementation of Voxel Cone Tracing with OpenGL and C++'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/Rasterizer',
+            img: 'project-sr.png',
+            title: 'Software Rasterizer',
+            body: 'Software Rasterizer written in C++ to learn how Graphics API work'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/PBR',
+            img: 'project-pbr.png',
+            title: 'PBR',
+            body: 'Implementing Physically based rendering with morden graphics API'
+        },
+        {
+            href: 'https://github.com/Guo-Haowei/TheAviator',
+            img: 'project-the-aviator.png',
+            title: 'The Aviator',
+            body: 'My first game written with OpenGL and C++'
+        },
+        {
+            href: 'pages/SuperCaveBoy/index.html',
+            img: 'project-super-caveboy.png',
+            title: 'Super Cave Boy',
+            body: 'A 2D platformer game written in JavaScript'
+        },
+        {
+            href: 'pages/TurnBasedGame/index.html',
+            img: 'project-turn-based.png',
+            title: 'Turn Based Game',
+            body: 'A turn based RPG game written in JavaScript'
         }
-        _data = tmp;
+    ];
+    let cardList = [];
+    for (let i = 0; i < cards.length; i += 1) {
+        if (i % 3 === 0) {
+            cardList.push([]);
+        }
+        cardList[cardList.length - 1].push(cards[i]);
     }
 
-    return _data;
+    return cardList;
 };
